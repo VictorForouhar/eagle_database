f63bcfe1bf535ae95ad0245ed5db9abfb925e381
Victor Forouhar <victor.j.forouhar@durham.ac.uk>
2021-12-31 15:29:48 +0000

    Added method to load data

--- src/eagle_database/database_file.py
+++ src/eagle_database/database_file.py
@@ -14,7 +14,6 @@ class database():
         Returns
         --------
         None
-
         """
         
         # Path to database
@@ -25,3 +24,24 @@ class database():
         
         # Dict where data is stored
         self.data = {}
+    
+
+    def load(self, group_name):
+        """
+        Helper function used to load data if it is not available yet.
+
+        Parameters
+        -----------
+        group_name : str
+            Name of the group we want to load data from
+
+        Returns
+        --------
+        None
+        """
+
+        if group_name not in self.data:
+            try: 
+                self.data[group_name] = self.file[group_name][()]
+            except:
+                raise KeyError('No group with specified name is prensent in this file.')
