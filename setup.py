dcd17d96571a0e840bdc04d3bae243df97a3c3c9
Victor Forouhar <victor.j.forouhar@durham.ac.uk>
2021-12-31 15:22:16 +0000

    Files for easy setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='eagle_database',
+      version='0.0.1',
+      description='Package used to read EAGLE-like Subfind databases',
+      author='Victor Forouhar Moreno',
+      author_email='victor.j.forouhar@durham.ac.uk',
+      url='https://github.com/VictorForouhar/eagle_database',
+      packages=['eagle_database'],
+     )
